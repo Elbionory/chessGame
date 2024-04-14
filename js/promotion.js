6d390e149d69495ef27b28acb3d5e98cf9ec627e
Elbionory <100975502+Elbionory@users.noreply.github.com>
2024-04-14 05:26:12 +0200

    Add files via upload

---
+++ js/promotion.js
@@ -0,0 +1,17 @@
+
+ export function promotion() {
+    document.querySelectorAll(`[data-piece='blackPawn']`).forEach(blackPawn => {
+        if (blackPawn.parentElement.dataset.y == 1) {
+            blackPawn.classList.remove('bg-blackPawn')
+            blackPawn.classList.add('bg-blackQueen')
+            blackPawn.setAttribute('data-piece', 'blackQueen')
+        }
+    })
+    document.querySelectorAll(`[data-piece='whitePawn']`).forEach(whitePawn => {
+        if (whitePawn.parentElement.dataset.y == 8) {
+            whitePawn.classList.remove('bg-whitePawn')
+            whitePawn.classList.add('bg-whiteQueen')
+            whitePawn.setAttribute('data-piece', 'whiteQueen')
+        }
+    })
+}
